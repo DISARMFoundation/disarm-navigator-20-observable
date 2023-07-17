691019338ea2fd10e5bfeb314f5ffebb25e56ca0
VVX7 <info@vvx7.io>
2023-07-17 11:38:33 -0400

    add gh-pages

--- nav-app/src/app/classes/index.ts
+++
@@ -1,8 +0,0 @@
-export { VersionChangelog } from "./version-changelog";
-export { Version } from "./version";
-export { ContextMenuItem } from "./context-menu-item";
-export { Gradient, Gcolor} from "./gradient";
-export { Filter } from "./filter";
-export { LayoutOptions } from "./layout-options";
-export { Link } from "./link";
-export { Metadata } from "./metadata";
